e61a8d5330b4c4c7034cd4654e9909c727379177
Shujaat Azim <shujaat.azim@gmail.com>
2021-06-11 18:54:35 -0400

    Built chart for last 30 days of bitcoin price. Will add filters.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Chart from './Chart';
 
 const App = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.coindesk.com/v1/bpi/historical/close.json")
+    .then(resp => resp.json())
+    .then(d => setData(Object.keys(d.bpi).map(date => {
+      return {
+        date: new Date(date),
+        price: d.bpi[date]
+      };
+    })))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div>
-
+      <h2 style={{ textAlign: "center" }}>30 Day Bitcoin Price Chart</h2>
+      <Chart data={data} />
     </div>
   );
 }
