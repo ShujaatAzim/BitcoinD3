31fd16bf6cbe555d8fd46646a8e4a438354e61bd
Shujaat Azim <shujaat.azim@gmail.com>
2021-06-14 22:04:15 -0400

    Added interactivity to chart, allowing for filtering of date range of user's choice. Changed styling to match and improve UX.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import BitcoinChart from './BitcoinChart';
 const App = () => {
 
   const [data, setData] = useState([])
+  const [clicked, setClicked] = useState(false)
+  const [filter, setFilter] = useState(30)
 
   useEffect(() => {
+    getData()
+    // eslint-disable-next-line
+  }, [])
+
+  const getData = () => {
     fetch("https://api.coindesk.com/v1/bpi/historical/close.json")
     .then(resp => resp.json())
     .then(d => setData(Object.keys(d.bpi).map(date => {
@@ -15,12 +22,29 @@ const App = () => {
       };
     })))
     .catch(error => console.log(error))
-  }, [])
+  }
 
   return (
-    <div style={{ backgroundColor: "lightcyan", minHeight: "100vh" }}>
-      <h2 style={{ textAlign: "center" }}>30 Day Bitcoin Price Chart</h2>
-      <BitcoinChart data={data} />
+    <div style={{ textAlign: "center", backgroundColor: "lightcyan", minHeight: "100vh", minWidth: "100vw" }}>
+      <h2 style={{ textAlign: "center" }}>Interactive BitCoin Price Chart</h2>
+      <BitcoinChart filter={filter} data={data} />
+      <br />
+      <br />
+      <h4>Some filters:</h4>
+      <div style={{ marginLeft: "20vw", marginRight: "20vw", display: "flex", justifyContent: "space-around" }}>
+        { clicked ? 
+        <div>
+          <button disabled={!clicked} onClick={() => {setData(getData);setClicked(false);setFilter(30)}}>Reset Filters</button>
+        </div> :
+        <div>
+          <button disabled={clicked} onClick={() => {setData(data.slice(-5));setClicked(true);setFilter(5)}}>Last 5 Days</button>
+          <button disabled={clicked} onClick={() => {setData(data.slice(-10));setClicked(true);setFilter(10)}}>Last 10 Days</button>
+          <button disabled={clicked} onClick={() => {setData(data.slice(-15));setClicked(true);setFilter(15)}}>Last 15 Days</button>
+          <button disabled={clicked} onClick={() => {setData(data.slice(-20));setClicked(true);setFilter(20)}}>Last 20 Days</button>
+          <button disabled={clicked} onClick={() => {setData(data.slice(-25));setClicked(true);setFilter(25)}}>Last 25 Days</button>
+        </div>
+        }
+      </div>
     </div>
   );
 }
--- src/BitcoinChart.js
+++ src/BitcoinChart.js
@@ -76,7 +76,7 @@ class Chart extends React.Component {
       <svg width={width} height={height}>
         <path id={"line"} d={line(data)} stroke="#6788ad" fill="transparent" />
         <path id={"area"} d={area(data)} fill="#6788ad" style={{ opacity: 0.2 }} />
-        <text transform={`translate(${width / 2 - margin.left -margin.right}, ${height - 10})`}>Dates for the last 30 days</text>
+        <text transform={`translate(${width / 2 - margin.left -margin.right}, ${height - 10})`}>Dates for the Last {this.props.filter} Days</text>
         <text transform={`translate(15, ${(height - margin.bottom) /1.5}) rotate(270)`}>Amount in USD</text>
         <g ref="xAxis" transform={`translate(0, ${height - margin.bottom})`} />
         <g ref="yAxis" transform={`translate(${margin.left}, 0)`} />
